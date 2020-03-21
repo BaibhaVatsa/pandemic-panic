599c0705784bb0db1042b7a252b9063216b7d1be
Baibhav Vatsa <baibhavvatsa@gmail.com>
2020-03-21 04:01:08 -0500

    added declarations for functions and data types

--- docs/SYSTEM.md
+++ docs/SYSTEM.md
@@ -10,7 +10,6 @@ Keeps constantly checking for new updates and when there are, emails all clients
 Keeps only a list of sources with email ids to them
 Expose a subscribe API for clients to push to add email ids
 
-
 ### Client
 
 
--- src/server.go
+++ src/server.go
@@ -3,7 +3,42 @@ package server
 import (
 	"net/http"
 	"os"
+	"fmt"
 	"strings"
 )
 
+type Source struct {
+	Name	string
+	URL		string
+}
+
+type Subscriber string
+
+type Article struct {
+	Title			string
+	Publisher	Source
+	URL				string
+}
+
+var subscribers = make(map[Source][]Subscriber)
+
+func (src *Source) addSubscriber(sub Subscriber) error {
+	subscribers[src] = append(subscribers[src], sub)
+}
+
+func (src *Source) removeSubscriber(sub Subscriber) error {
+
+}
+
+func (art *Article) updateSubscribers() error {
+	src := art.Publisher
+	subs := subscribers[src]
+	for _, sub := range subs {
+		sub.notify(art)
+	}
+}
+
+func (sub *Subscriber) notify(art Article) error {
+	
+}
 
